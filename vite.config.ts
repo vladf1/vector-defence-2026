d8a8fbe321bb671c46e03e5bc4fe0b7ddbc83150
Vlad F <50790065+vladf1@users.noreply.github.com>
2026-04-22 12:33:03 -0700

    Split vendor bundle

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,15 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
 });
