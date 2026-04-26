377fe1b00e7d72887ba54c85191654194bc00805
Vlad F <50790065+vladf1@users.noreply.github.com>
2026-04-26 08:14:29 -0700

    make ball monsters look like pac men

--- src/entities/monsters/ball-monster.ts
+++ src/entities/monsters/ball-monster.ts
@@ -6,6 +6,7 @@ const SPEED_PER_SECOND = 81;
 const HIT_POINTS = 220;
 const BOUNTY = 20;
 const RADIUS = 7.5;
+const MOUTH_ANGLE = Math.PI * 0.18;
 
 export class BallMonster extends Monster {
   constructor(path: Point[]) {
@@ -13,9 +14,16 @@ export class BallMonster extends Monster {
   }
 
   protected drawBody(context: CanvasRenderingContext2D): void {
+    context.rotate(this.angle);
     context.beginPath();
-    context.arc(0, 0, this.radius, 0, Math.PI * 2);
+    context.moveTo(0, 0);
+    context.arc(0, 0, this.radius, MOUTH_ANGLE, (Math.PI * 2) - MOUTH_ANGLE);
+    context.closePath();
     context.fill();
     context.stroke();
+
+    context.beginPath();
+    context.arc(this.radius * 0.12, -this.radius * 0.5, this.radius * 0.16, 0, Math.PI * 2);
+    context.fill();
   }
 }
